841162946164b8a00a38cce85c26af4d9acfc11c
Anusha-Mandalapu <32685058+Anusha-Mandalapu@users.noreply.github.com>
2020-10-16 19:02:05 +0530

    deleted package-lock.json

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export class Hello extends Component {
     render() {
-        return React.createElement(`h1`,{id:'hello', className:'dummy'},"Hello Anusha!");
+        return React.createElement(`h1`,{id:'hello', className:'dummy'} ,"Hello Anusha!");
     }
 }
 export default Hello;
