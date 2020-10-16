2f9eed4dfbb5c272b3a8a27c2357c6a85e6b34bf
Anusha-Mandalapu <32685058+Anusha-Mandalapu@users.noreply.github.com>
2020-10-16 17:19:34 +0530

    add: initialCommit

---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+/*function Greet() {
+  return (
+    <h1>
+      Hello Anusha!
+    </h1>
+  );
+}*/
+export const Greet= () =>{
+    return ( <h1>
+      Hello Anusha!
+    </h1>);
+}
+//export default Greet;
